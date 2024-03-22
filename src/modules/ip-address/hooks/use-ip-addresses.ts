79c5d0c2f480231548d5515290bb0e8c85d7cb10
ihorbalan <i.balan@seedium.io>
2024-03-22 14:08:26 +0200

    feat: add table connection to api

--- src/modules/ip-address/hooks/use-ip-addresses.ts
+++ src/modules/ip-address/hooks/use-ip-addresses.ts
@@ -1,54 +1,88 @@
-import { UseQueryOptions, useQuery } from '@tanstack/react-query';
-import { AxiosError } from 'axios';
+import {
+  useInfiniteQuery,
+  UseInfiniteQueryOptions,
+  InfiniteData,
+  QueryKey,
+} from '@tanstack/react-query';
+import axios, { AxiosError } from 'axios';
 import type { DefaultError } from '@tanstack/query-core';
 import { IList } from 'modules/core/types';
 import { IPAddress } from 'modules/ip-address/types';
-import { mockData1 } from 'modules/ip-address/helpers/mocks';
+import queryString from 'query-string';
 
 export const IP_ADDRESSES_QUERY_KEY = 'ip-addresses';
 
 export type ListIPAddressOptions = {
   q?: string;
-  sort: string;
-  limit?: number;
+  sort?: string;
+  limit: number;
   offset?: number;
 };
 
 export const useIpAddresses = (
-  options?: ListIPAddressOptions,
+  options: ListIPAddressOptions,
   queryOptions?: Omit<
-    UseQueryOptions<IList<IPAddress>, AxiosError<DefaultError>>,
+    UseInfiniteQueryOptions<
+      IList<IPAddress>,
+      AxiosError<DefaultError>,
+      InfiniteData<IList<IPAddress>>,
+      IList<IPAddress>,
+      QueryKey,
+      object
+    >,
     'queryFn' | 'queryKey'
   >
 ) => {
-  return useQuery<IList<IPAddress>, AxiosError<DefaultError>>({
+  return useInfiniteQuery<
+    IList<IPAddress>,
+    AxiosError<DefaultError>,
+    InfiniteData<IList<IPAddress>>,
+    QueryKey,
+    object
+  >({
     queryKey: [IP_ADDRESSES_QUERY_KEY, options],
     staleTime: 1000 * 60 * 5,
-    queryFn: async () => {
-      // TODO: Implement API call when issue with Api Token is resolved
-      // const queryParams = queryString.stringify(
-      //   {
-      //     ...options,
-      //   },
-      //   {
-      //     arrayFormat: 'bracket',
-      //     encode: false,
-      //   }
-      // );
-      //
-      // const videos = await axios.get<IList<IPAddress>>(
-      //   `/ipam/ip-addresses?${queryParams}`
-      // );
-      //
-      // return videos.data;
-      return new Promise<IList<IPAddress>>((resolve) => {
-        const responseTime = Math.random() * 1000;
-
-        setTimeout(() => {
-          resolve(mockData1);
-        }, responseTime);
-      });
+    initialPageParam: {
+      limit: options.limit,
+      offset: 0,
     },
+    queryFn: async ({ pageParam }) => {
+      const queryParams = queryString.stringify(
+        {
+          ...options,
+          ...pageParam,
+        },
+        {
+          arrayFormat: 'bracket',
+          encode: false,
+        }
+      );
+
+      const ipAddresses = await axios.get<IList<IPAddress>>(
+        `/ipam/ip-addresses?${queryParams}`
+      );
+
+      return ipAddresses.data;
+    },
+    getNextPageParam: (lastPage) => {
+      if (lastPage.next) {
+        const res = { ...queryString.parse(lastPage.next.split('?')[1]) };
+
+        return { limit: Number(res?.limit), offset: Number(res?.offset) };
+      }
+
+      return {};
+    },
+    getPreviousPageParam: (prevPage) => {
+      if (prevPage.previous) {
+        const res = { ...queryString.parse(prevPage.previous.split('?')[1]) };
+
+        return { limit: Number(res?.limit), offset: Number(res?.offset) };
+      }
+
+      return {};
+    },
+
     ...queryOptions,
   });
 };
--- src/providers/axios-provider/axios-provider.tsx
+++ src/providers/axios-provider/axios-provider.tsx
@@ -3,7 +3,7 @@ import { AxiosProviderProps } from './axios-provider.interface';
 import axios from 'axios';
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
-axios.defaults.headers.common['API-Key'] = process.env.REACT_APP_API_TOKEN;
+axios.defaults.headers.common.Authorization = `Token ${process.env.REACT_APP_API_TOKEN}`;
 
 export const AxiosProvider: FC<AxiosProviderProps> = ({ children }) => {
   return children;
