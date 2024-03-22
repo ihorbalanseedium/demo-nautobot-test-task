dad7899be0afc613162b9c7e5caf6be5ea9a3187
ihorbalan <i.balan@seedium.io>
2024-03-22 14:41:26 +0200

    feat: add table search

--- src/pages/home/home.styled.ts
+++ src/pages/home/home.styled.ts
@@ -1,4 +1,4 @@
-import { Box, styled, Typography } from '@mui/material';
+import { Box, Button, styled, TextField, Typography } from '@mui/material';
 
 export const Root = styled(Box)`
   text-align: center;
@@ -15,4 +15,36 @@ export const ActionWrapper = styled(Box)`
   margin-top: 20px;
   display: flex;
   justify-content: flex-end;
+`;
+
+export const SearchWrapper = styled(Box)`
+  margin-top: 20px;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const TextInputStyled = styled(TextField)<{ error?: boolean }>`
+  .MuiInputBase-root {
+    background-color: #000;
+    color: #c8c8c8;
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+
+  .MuiInputBase-input {
+    padding: 8px;
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+
+  .Mui-focused {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+    border-right-width: 0;
+  }
+`;
+
+export const SearchButton = styled(Button)`
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
 `;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '@mui/material';
+import { GridSearchIcon, GridAddIcon } from '@mui/x-data-grid';
 import { NoticeBlock } from 'modules/core/components';
 import {
   IPAddressesTable,
@@ -7,21 +8,49 @@ import {
 } from 'modules/ip-address/components';
 import { useModal } from 'modules/core/hooks/use-modal';
 
-import { ActionWrapper, Root, Title } from './home.styled';
+import {
+  ActionWrapper,
+  Root,
+  SearchButton,
+  SearchWrapper,
+  TextInputStyled,
+  Title,
+} from './home.styled';
 
 export const Home = () => {
   const { open, handleOpen, handleClose } = useModal();
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('');
+
+  const handleSearchPress = () => {
+    setQuery(search);
+  };
 
   return (
     <Root>
       <NoticeBlock />
+      <SearchWrapper>
+        <TextInputStyled
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <SearchButton
+          variant='contained'
+          color='primary'
+          onClick={handleSearchPress}
+        >
+          <GridSearchIcon />
+        </SearchButton>
+      </SearchWrapper>
       <ActionWrapper>
         <Button variant='contained' color='primary' onClick={handleOpen}>
+          <GridAddIcon />
           Add
         </Button>
       </ActionWrapper>
+
       <Title variant='h5'>IP Addresses</Title>
-      <IPAddressesTable />
+      <IPAddressesTable q={query} />
       <AddIpAddressModal open={open} handleClose={handleClose} />
     </Root>
   );
